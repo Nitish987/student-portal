cdaf37f25e1a89e605415cfc1f1937f32e13c90e
Nitish Kumar <nk98721000@gmail.com>
2023-02-16 19:47:07 +0530

    logo added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,29 @@
 import React from 'react'
+import logo from '../logo.svg';
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand text-dark" href="/">RKGITSP</a>
+        <a class="navbar-brand text-light" href="/">
+          <img src={logo} alt="Logo" width="29" height="29" class="d-inline-block align-text-top" />
+          RKGITSP
+        </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active text-dark" aria-current="page" href="/">Home</a>
+              <a className="nav-link active text-light" aria-current="page" href="/">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-dark" href="/">Link</a>
+              <a className="nav-link text-light" href="/">Link</a>
             </li>
           </ul>
           <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-success" type="submit">Search</button>
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-success" type="submit">Search</button>
           </form>
         </div>
       </div>
