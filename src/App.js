59586c443e60eb5c11c533673d570a318c1c8849
Nitish Kumar <nk98721000@gmail.com>
2023-02-16 13:11:00 +0530

    navbar component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Alert from "./components/Alert";
+import Profile from "./components/Profile";
+import Dashboard from "./components/Dashboard";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Alert/>
+      <Alert />
+      <Navbar/>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Dashboard/>}/>
+          <Route path="/" element={<Dashboard/>}/>
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
