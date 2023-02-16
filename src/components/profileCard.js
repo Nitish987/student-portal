27a8f1cfb7374c4c5c5824c7218a4af4c8659569
Himanshu Chaudhary <him.chaudhary212@gmail.com>
2023-02-16 20:31:08 +0530

    profile card

---
+++ src/components/profileCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function ProfileCard() {
+    return (
+        <div className="cont container-fluid d-flex justify-content-center">
+            <div className="top-profile d-flex p-2">
+                <div className="profile-cont"></div>
+                <div className="info d-flex flex-column">
+                    <div className="profile-ul ms-5 h2 mb-0">Himanshu</div>
+                    <div className="profile-ul ms-5 text-muted">student</div>
+                    <div className="profile-ul ms-5 d-flex justify-content-start rounded-3">
+                        <div>
+                            <p className="small mb-1">certificate</p>
+                            <p className="mb-0">41</p>
+                        </div>
+                        <div className="px-3">
+                            <p className="small mb-1">Followers</p>
+                            <p className="mb-0">--</p>
+                        </div>
+                        <div>
+                            <p className="small mb-1">Rating</p>
+                            <p className="mb-0">--</p>
+                        </div>
+                    </div>
+                    <div className="profile-ul d-flex pt-1 ms-5">
+                        <button type="button" className="btn btn-primary btn-sm">change profile</button>
+                        <button type="button" className="btn btn-primary btn-sm ms-1">change branch</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
