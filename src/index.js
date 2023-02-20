995f467ab1e7e2b835a52b8df2441d4383bb517a
Shivang <harshsaini5656@gmail.com>
2023-02-20 14:28:03 +0530

    update spaces

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import App from './App';
 import { store } from './app/store'
 import { Provider } from 'react-redux'
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
